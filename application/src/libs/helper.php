8b638323406806802ef30c9a5c71dc94bab9c99b
Klaustry <ooo-tarasov@ya.ru>
2021-09-08 21:18:42 +0300

    Update helper.php

--- application/src/libs/helper.php
+++ application/src/libs/helper.php
@@ -4,15 +4,6 @@ class Helper
 {
     /**
      * debugPDO
-     *
-     * Shows the emulated SQL query in a PDO statement. What it does is just extremely simple, but powerful:
-     * It combines the raw query and the placeholders. For sure not really perfect (as PDO is more complex than just
-     * combining raw query and arguments), but it does the job.
-     * 
-     * @author Panique
-     * @param string $raw_sql
-     * @param array $parameters
-     * @return string
      */
     static public function debugPDO($raw_sql, $parameters) {
 
@@ -37,15 +28,6 @@ class Helper
                 $values[$key] = 'NULL';
             }
         }
-
-        /*
-        echo "<br> [DEBUG] Keys:<pre>";
-        print_r($keys);
-        
-        echo "\n[DEBUG] Values: ";
-        print_r($values);
-        echo "</pre>";
-        */
         
         $raw_sql = preg_replace($keys, $values, $raw_sql, 1, $count);
 
